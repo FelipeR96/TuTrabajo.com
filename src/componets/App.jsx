701d29c65793d0162d08c73a7d6c45a275b57fed
mssm1604 <smonsalve386@gmail.com>
2024-01-03 19:55:37 -0500

    first commit

---
+++ src/componets/App.jsx
@@ -0,0 +1,17 @@
+import { Navbar } from './header/navbar'
+
+function App() {
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/componets/header/navbar.jsx
@@ -0,0 +1,12 @@
+export function Navbar() {
+  return (
+    <nav className='navbar'>
+      <div className='hero'>TuTrabajo.com</div>
+      <ul>
+        <li>Change theme icon</li>
+        <li> user icon</li>
+        <li>cart icon</li>
+      </ul>
+    </nav>
+  )
+}
